578568cc2e9b5070389770f45ae8dc8e6aa8f3ba
Andres Fanjul <afanjul@contractor.appsbees.com>
2021-08-12 01:48:48 -0400

    made use of bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Carousel from 'react-bootstrap/Carousel'
+
+import Contact from './Components/Contact';
+import Menu from './Components/Menu';
+import Suggestions from './Components/Suggestions';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <div className="container-fluid">
+          <Link to="/" className="navbar-brand">Home</Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item">
+                  <Link to="/Menu" className="nav-link active">Menu</Link>
+                </li>
+                <div className="nav-item">
+                  <h3 style={{color:"white"}}>Excellent Cakes</h3>
+                </div>
+                <li className="nav-item">
+                  <Link to="/Suggestions" className="nav-link active">Sugerencias</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/Contact" className="nav-link active">Contactos</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <Switch>
+          <Route path="/Contact"><Contact/></Route>
+          <Route path="/Suggestions"><Suggestions/></Route>
+          <Route path="/Menu"><Menu/></Route>
+        </Switch>
+        <div>
+          
+        </div>
+    
+
+      </div>
+    </Router>
   );
 }
 

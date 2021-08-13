ff18bd2a8509b25e772d48303489a74ee3dfcdc1
Andres Fanjul <afanjul@contractor.appsbees.com>
2021-08-13 02:22:36 -0400

    changed colors

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,9 +5,9 @@ import brownLine from '../assets/images/linea marron.png'
 const Contact = () => {
     return (
         <div className="mt-4" style={{ textAlign: 'center' }}>
-            <h1 style={{ fontFamily: 'Leckerli One' }}>¡Visitanos y Contáctanos!</h1>
+            <h1 style={{ fontFamily: 'Leckerli One', color:'#3E2F25' }}>¡Visitanos y Contáctanos!</h1>
             <img src={brownLine} alt='' style={{ width: 600 }} />
-            <h2 className='mt-3 border-bottom border-dark' style={{ fontFamily: 'Poppins' }}>Tenemos 5 sucursales alrededor de Sto. Dgo.</h2>
+            <h2 className='mt-3 border-bottom border-dark' style={{ fontFamily: 'Poppins', color:'#3E2F25'       }}>Tenemos 5 sucursales alrededor de Sto. Dgo.</h2>
             <div className="row form-group" style={{ backgroundColor: '#BA9A77' }}>
                 <div className="col-md-4 mx-4 mt-3">
                 </div>

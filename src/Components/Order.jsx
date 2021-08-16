ab05d670ee1b0a6d6f2c36f24aeeda898c31b2fb
Andres Fanjul <aefe15@gmail.com>
2021-08-15 23:43:54 -0400

    .

--- src/Components/Order.jsx
+++ src/Components/Order.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
 
 
-import dessert_product1 from '../assets/images/producto_postre1.png'
 
-const Order = ({ img }) => {
+const Order = () => {
     return (
         <div className="container marketing mt-5">
-            <img className='rounded-circle' style={{ width: 300, height: 300 }} src={dessert_product1} alt='' />
-            <h2 className='float-end ml-3'>Estas ordenando</h2>
+
+            {/* <img className='rounded-circle' style={{ width: 300, height: 300 }} src={img} alt='' />
+            <h2 className='float-end ml-3'>Estas ordenando {title} {id}</h2>
+            <p>{description}</p> */}
         </div>
     )
 }
